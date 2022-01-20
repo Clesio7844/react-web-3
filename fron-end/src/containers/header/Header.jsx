6dee54cd3251553c63b6300cf94fccee081cd045
david <daviddecastro42@live.co.uk>
2022-01-20 15:57:16 +0000

    finished the header component

--- fron-end/src/containers/header/Header.jsx
+++ fron-end/src/containers/header/Header.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './Header.css';
+import people from '../../assets/people.png';
+import ia from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>
-      <h1>header</h1>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpeasing. Not
+          thought all excerise blessing. indulgence way everything joy
+          alteration boisterous
+        </p>
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt='people' />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className='gpt3__header-image'>
+        <img src={ia} alt='ai' />
+      </div>
     </div>
   );
 };

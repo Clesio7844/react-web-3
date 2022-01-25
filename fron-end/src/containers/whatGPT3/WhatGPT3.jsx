9e7ab8b427dc2c3473194cc64a68c3c10afe7354
david <daviddecastro42@live.co.uk>
2022-01-25 00:06:31 +0000

    finished the whatgp3 componet and use prop to fetaurecomponent

--- fron-end/src/containers/whatGPT3/WhatGPT3.jsx
+++ fron-end/src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './WhatGPT3.css';
+import { Feature } from '../../component';
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      <h1>WhatGPT3</h1>
+    <div className='gpt3__whatgpt3 section__margin' id='whatp3'>
+      <div className='gpt3__whatgpt3-featured'>
+        <Feature title='What is GPT-3' text='We so opinion friends as delight. Whole front do of plate ne detective nor convinced residence owen. Connection has put impossiblity boisterrou. At jointure ladyship an insisted so humanity he'/>
+      </div>
+      <div className='gpt3__whatgp3-heading'>
+        <h1 className='gradient__text'>
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Feature title='Chatbots' text='We so oponion friends me message as delight. Whole front do of plate heard on ought'/>
+        <Feature title='Knowldgebase' text='At jointure ladyship an insisted so humanity he Friendly bachelor entrence to on by. As put impossible own apartmet to'/>
+        <Feature title='Education' text='At jointure ladyship an insisted so humanity he Friendly bachelor entrence to on by. As put impossible own apartmet to'/>
+      </div>
     </div>
   );
 };
